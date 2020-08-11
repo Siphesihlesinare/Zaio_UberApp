c09e21657b5c612822d037b384cddd7a9f91dccc
Siphesihlesinare <56110787+Siphesihlesinare@users.noreply.github.com>
2020-08-11 12:14:43 +0200

    Update Person.java

--- uberApp_OOP/src/Person.java
+++ uberApp_OOP/src/Person.java
@@ -16,17 +16,20 @@
 
 //  Person siphe = new Person("siphe", "sinare", "07838282", 123.12);
 // siphe.name = "Siphe2";
-//  Person siphe = new Person("Siphe", "Boddu", "07838282", 123.12);
+//  Person siphe = new Person("Siphe", "sinare", "07838282", 123.12);
 
 public class Person {
 
     //instance variables: attributes
+   //Instance variables hold values that must be referenced by more than one method or a constructor .
+  
     private String name;
     private String surname;
     private String phone_number;
     private double cash;
 
     //constructor
+    // the constructors are used to initialize the object class and also it does not have a return type
     Person(String name, String surname, String phone_number, double cash) {
         this.name = name;
         this.surname = surname;
@@ -37,6 +40,7 @@ public class Person {
     //methods: behaviour
     
     //setters
+    //setters is the method that can updated the value of the variable
     public void setName(String name) {
         this.name = name;
     }
@@ -54,6 +58,7 @@ public class Person {
     }
 
     //getters
+    //getters it a method that reads the value of tthe variable
     public String getName(){
         return this.name;
     }
@@ -69,7 +74,10 @@ public class Person {
     public double getCash(){
         return this.cash;
     }
-
+//toString method
+    //used to return the representation of an object
+    //it is used to return name of the person and thier surname
+    
     public String toString() {
         return this.name + " " + this.surname;
     }
